6fd7da40820145b0b4c2d77953c05c98cb2de66d
Hamid Oujand <hmdrzaa11@gmail.com>
2021-09-26 20:46:34 +0330

    add 404 route handler

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let usersRouter = require("./routes/userRoutes");
+let AppError = require("./utils/apiErrors");
 
 let app = express();
 
@@ -7,5 +8,10 @@ let app = express();
 app.use(express.json());
 
 app.use("/api/v1/users", usersRouter);
+
+//404 route
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can not file ${req.originalUrl} on this server`, 404));
+});
 
 module.exports = app;
