1f4c4b743ee4f00dd095b5fd3e007ba91e9cadac
Hamid Oujand <hmdrzaa11@gmail.com>
2021-09-26 20:13:28 +0330

    add body parser middleware

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let usersRouter = require("./routes/userRoutes");
 
 let app = express();
 
+//middlewares
+app.use(express.json());
+
 app.use("/api/v1/users", usersRouter);
 
 module.exports = app;
