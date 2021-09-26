2c3d1b8053a30535ae43abe4dc3ca93d27704c81
Hamid Oujand <hmdrzaa11@gmail.com>
2021-09-26 20:25:09 +0330

    remove password from res body

--- models/User.js
+++ models/User.js
@@ -2,55 +2,64 @@ let { Schema, model } = require("mongoose");
 let { default: validator } = require("validator");
 let bcrypt = require("bcryptjs");
 
-let userSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, "username is required"],
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: [true, "email is required"],
-    lowercase: true,
-    unique: true,
-    validate: {
-      validator(value) {
-        return validator.isEmail(value);
-      },
-      message(props) {
-        return `"${props.value}" is not a valid email address`;
+let userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      lowercase: true,
+      unique: true,
+      validate: {
+        validator(value) {
+          return validator.isEmail(value);
+        },
+        message(props) {
+          return `"${props.value}" is not a valid email address`;
+        },
       },
     },
-  },
 
-  password: {
-    type: String,
-    required: [true, "password is required"],
-    minlength: [8, "password must be at least 8 chars length"],
-    trim: true,
-  },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      minlength: [8, "password must be at least 8 chars length"],
+      trim: true,
+    },
 
-  passwordConfirm: {
-    type: String,
-    required: [true, "passwordConfirm is required"],
-    validate: {
-      validator(value) {
-        return value === this.password;
+    passwordConfirm: {
+      type: String,
+      required: [true, "passwordConfirm is required"],
+      validate: {
+        validator(value) {
+          return value === this.password;
+        },
+        message: "password and passwordConfirm do not match",
       },
-      message: "password and passwordConfirm do not match",
+    },
+    photo: String,
+    passwordChangedAt: Date,
+    role: {
+      type: String,
+      enum: {
+        values: ["user", "admin"],
+        message: "invalid role",
+        default: "user",
+      },
     },
   },
-  photo: String,
-  passwordChangedAt: Date,
-  role: {
-    type: String,
-    enum: {
-      values: ["user", "admin"],
-      message: "invalid role",
-      default: "user",
+  {
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret["password"];
+      },
     },
-  },
-});
+  }
+);
 
 //hash password
 
