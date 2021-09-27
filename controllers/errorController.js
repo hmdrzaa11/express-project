2a91209f9ec33dde8454e654fa54ccc573dee7ec
Hamid Oujand <hmdrzaa11@gmail.com>
2021-09-27 15:16:57 +0330

    add better development error handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,20 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+let sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
     err: err,
+    stack: err.stack,
   });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    console.log("here");
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    //handle production
+  }
 };
