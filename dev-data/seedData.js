497471956dd4fb4d7080944533ea458f3eb47e27
Hamid Oujand <hmdrzaa11@gmail.com>
2021-09-27 22:04:02 +0330

    added a function to seed DB

--- dev-data/seedData.js
+++ dev-data/seedData.js
@@ -1,5 +1,7 @@
 let faker = require("faker");
 let path = require("path");
+let Tour = require("../models/Tour");
+
 require("dotenv").config({
   path: path.join(__dirname, "../.env"),
 });
@@ -15,4 +17,46 @@ mongoose
 
 let command = process.argv[2];
 
-console.log("all good", command);
+let seedTourDb = async () => {
+  let tours = Array.from({ length: 10 }).map((_) =>
+    Tour.create({
+      name: faker.name.title(),
+      duration: faker.datatype.number(20),
+      maxGroupSize: faker.datatype.number(20),
+      difficulty: faker.random.arrayElement(["easy", "difficult", "medium"]),
+      ratingsAverage: faker.datatype.float({ min: 1, max: 5, precision: 1 }),
+      price: faker.finance.amount(),
+      summary: faker.lorem.paragraph(3),
+      imageCover: faker.image.imageUrl(),
+      startLocation: {
+        type: "Point",
+        coordinates: [faker.address.longitude(), faker.address.latitude()],
+        address: faker.address.cityName(),
+        description: faker.lorem.paragraph(),
+      },
+    })
+  );
+  await Promise.all(tours);
+};
+
+if (command === "--import") {
+  (async function () {
+    try {
+      await seedTourDb();
+      process.exit(0);
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  })();
+} else if (command === "--delete") {
+  (async function () {
+    try {
+      await Tour.deleteMany({});
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  })();
+}
