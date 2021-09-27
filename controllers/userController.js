4bf16e39b9b953688f2d82a79131e58448b334ef
Hamid Oujand <hmdrzaa11@gmail.com>
2021-09-27 14:31:45 +0330

    add route restriction

--- controllers/userController.js
+++ controllers/userController.js
@@ -78,3 +78,14 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = user;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("you don't have permission to perform this action", 403)
+      );
+    }
+    next();
+  };
+};
