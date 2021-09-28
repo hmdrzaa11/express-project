2c841ea2bf2a348806761a939de461829c824daa
Hamid Oujand <hmdrzaa11@gmail.com>
2021-09-28 22:55:59 +0330

    added rate limit to the api

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let express = require("express");
+let rateLimit = require("express-rate-limit");
 let AppError = require("./utils/apiErrors");
 let globalErrorHandler = require("./controllers/errorController");
 let usersRouter = require("./routes/userRoutes");
@@ -7,6 +8,14 @@ let tourRouter = require("./routes/tourRoutes");
 let app = express();
 
 //middlewares
+
+app.use(
+  rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "to many request from this IP, please try in 1 hour later",
+  })
+);
 app.use(express.json());
 
 app.use("/api/v1/users", usersRouter);
