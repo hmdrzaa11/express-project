2a16c60712ffbfe72ce4849cd993a2455eb46ca4
Hamid Oujand <hmdrzaa11@gmail.com>
2021-09-28 20:30:08 +0330

    added ApiFeature class

---
+++ utils/ApiFeature.js
@@ -0,0 +1,23 @@
+module.exports = class ApiFeatures {
+  constructor(mongooseQuery, queryParams) {
+    this.mongooseQuery = mongooseQuery;
+    this.queryParams = queryParams;
+  }
+  filter() {
+    let queyCopy = { ...this.queryParams };
+    //remove invalid params
+    let invalids = ["sort", "page", "limit", "fields"];
+    for (let key of invalids) {
+      delete queyCopy[key];
+    }
+
+    //turn them into "$gt"
+    let filterString = JSON.stringify(queyCopy).replace(
+      /\b(gte|gt|lt|lte)\b/g,
+      (matched) => `$${matched}`
+    );
+    let filterObj = JSON.parse(filterString);
+    this.mongooseQuery = this.mongooseQuery.find(filterObj);
+    return this;
+  }
+};
