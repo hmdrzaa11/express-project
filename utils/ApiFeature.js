d3326048dc8a888901e554cd1bfe5cc2ed9d5ea7
Hamid Oujand <hmdrzaa11@gmail.com>
2021-09-28 21:53:19 +0330

    added paginate method to ApiFeature

--- utils/ApiFeature.js
+++ utils/ApiFeature.js
@@ -32,12 +32,19 @@ module.exports = class ApiFeatures {
   }
 
   limitFields() {
-    if (this.queryParams.limit) {
-      let fields = this.queryParams.limit.split(",").join(" ");
+    if (this.queryParams.fields) {
+      let fields = this.queryParams.fields.split(",").join(" ");
       this.mongooseQuery = this.mongooseQuery.select(fields);
     } else {
       this.mongooseQuery = this.mongooseQuery.select("-__v");
     }
+    return this;
+  }
+  paginate() {
+    let page = this.queryParams.page * 1 || 1;
+    let limit = this.queryParams.limit * 1 || 10;
+    let skip = (page - 1) * limit;
+    this.mongooseQuery = this.mongooseQuery.skip(skip).limit(limit);
     return this;
   }
 };
